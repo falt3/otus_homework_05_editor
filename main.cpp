4ef523e0162db0d0a76a2e32e908349786cfe9e6
Lipatkin <lipatkin@scaegroup.com>
2023-09-12 00:13:22 +0300

    2

--- main.cpp
+++ main.cpp
@@ -6,100 +6,102 @@
 
 
 //----------------------------------------------------------------
-
+/**
+ * @brief Интерфейс для графических примитивов
+ * 
+ */
 class IPrimitive {
 public:
-    IPrimitive(const std::string& name) : m_name(name) { /*m_coord {0, 0};*/ }
-    // void setCenter(int x, int y) {};
-    virtual std::string print() = 0; //{ return "la-la-la"; }
+    IPrimitive(int id) : m_id(id) {}
+    virtual std::string print() = 0;
+    int getId() { return m_id; }
 protected:
-    std::string m_name;
-    // std::pair<int, int> m_coord;
+    int m_id;       ///< Идентификатор примитива
 };
 
+/**
+ * @brief Класс окружности
+ * 
+ */
 class Circle : public IPrimitive {
 public:
-    Circle(const std::string& name, int radius) : IPrimitive(name), m_radius(radius) {}
+    Circle(int id, int radius) : IPrimitive(id), m_radius(radius) {}
     virtual std::string print() { 
         std::ostringstream ss;
-        ss << "Circle: " << m_name << ", radius=" << m_radius;
+        ss << "Circle: " <<  "id=" << m_id << ", radius=" << m_radius;
         return ss.str(); 
     }
 private:
-    int m_radius;
+    int m_radius;   ///< Радиус окружности
 };
 
+/**
+ * @brief Класс прямоугольника
+ * 
+ */
 class Rectangle : public IPrimitive {
 public:
-    Rectangle(const std::string& name, int width, int height) : IPrimitive(name), m_width(width), m_height(height) {}
+    Rectangle(int id, int width, int height) : IPrimitive(id), m_width(width), m_height(height) {}
     virtual std::string print() { 
         std::ostringstream ss;
-        ss << "Rectangle: " << m_name << ", width=" << m_width << ", height=" << m_height;
+        ss << "Rectangle: " << "id=" << m_id << ", width=" << m_width << ", height=" << m_height;
         return ss.str(); 
     }
 private:
-    int m_width;
-    int m_height;
+    int m_width;        ///< Ширина прямоуголника
+    int m_height;       ///< Высота прямоугольника
 };
 
 //----------------------------------------------------------------
-
+/**
+ * @brief Класс документа
+ * @details Класс документа, содержит в себе имя документа и список графических примитивов
+ * 
+ */
 class Document {
 public:
-    Document(const std::string& name) : m_name(name) {}
-    void addPrimitive(std::unique_ptr<IPrimitive> obj) {
-        m_objects.push_back(std::move(obj));
-    }
-    void delPrimitive(std::string name) {}
+    Document(const std::string& name) : m_name(name) { std::cout << "document: constructor (" << name << ")\n"; }
+    ~Document() { std::cout << "document: destructor (" << m_name << ")\n"; }
+    void addPrimitive(std::unique_ptr<IPrimitive> obj) { m_objects.push_back(std::move(obj)); }
+    void delPrimitive(int id) { m_objects.remove_if([id](auto& el){ return (el->getId() == id); }); }
     const std::string& name() { return m_name; }
-    void print() {
+    void print() const {
          std::cout << "Документ: " << m_name << std::endl;
-         for (auto& el : m_objects) {
+         for (auto& el : m_objects)
             std::cout << "    " << el->print() << std::endl;
-         }
     }
+    int getId() { return ++m_id; }
 private:
-    std::string m_name;
-    std::list<std::unique_ptr<IPrimitive>> m_objects;
+    std::string m_name;     ///< Имя документа
+    std::list<std::unique_ptr<IPrimitive>> m_objects;   ///< Список графических примитивов
+    int m_id  = 0;          ///< Счетчик для получения нового идентификатора 
 };
 
 //----------------------------------------------------------------
-
+/**
+ * @brief Класс модели данных для работы с документами
+ * @details Класс модели данных для работы с документами, в данной реализации поддерживается только один открытый документ
+ * 
+ */
 class Model {
 public:
     void newDocument(const std::string& name) { m_doc.reset(new Document(name)); };
-    void newRectangle(const std::string& name, int width, int height) { 
-        if (m_doc.get() != nullptr) { 
-            Rectangle* r = new Rectangle(name, width, height);
-            m_doc->addPrimitive(std::unique_ptr<IPrimitive>(r)); 
-        } 
-    };
-    void newCircle(const std::string& name, int radius) {
-        if (m_doc.get() != nullptr) { 
-            Circle* c = new Circle(name, radius);
-            m_doc->addPrimitive(std::unique_ptr<IPrimitive>(c)); 
-        } 
-    }
+    void saveDocument() {};
+    void openDocument(const std::string& name) {};
 
-    void print() {
-       if (m_doc.get() != nullptr)
-        m_doc->print();
+    void newRectangle(int width, int height) { 
+        if (m_doc.get())
+            m_doc->addPrimitive(std::unique_ptr<IPrimitive>(new Rectangle(m_doc->getId(), width, height))); 
     };
-
+    void newCircle(int radius) {
+        if (m_doc.get()) 
+            m_doc->addPrimitive(std::unique_ptr<IPrimitive>(new Circle(m_doc->getId(), radius))); 
+    }   
+    void deleteObject(int id) { if (m_doc.get()) m_doc->delPrimitive(id); }
+    void print() const { if (m_doc.get()) m_doc->print(); };
 private:
-    std::unique_ptr<Document> m_doc;
-    // Document dd     {"asd"};
-};
-
-//----------------------------------------------------------------
-
-class View {
-public:
-    View() {};
-    void view(Model& model) {
-        model.print();
-    };
-private:
+    std::unique_ptr<Document> m_doc;    ///< текущий открытый документ
+    bool m_editFlag;
 };
 
 //----------------------------------------------------------------
@@ -107,26 +109,91 @@ private:
 class Controller {
 public: 
     Controller(Model& model) : m_model(model) { std::cout << "Controller: constructor\n"; }
-    void setView(View* view) { m_view = view; }
     ~Controller() { std::cout << "Controller: destructor\n"; }
-    void execute();
+    void test1();
+    void command(std::string line);
+    const Model& getModel() {return m_model;}
 private:
-    void newDocument(std::string name) {};
-    void openDocument(std::string name) {};
-    void saveDocument(std::string name) {};
-
+    // void newDocument(std::string name) {};
+    // void openDocument(std::string name) {};
+    // void saveDocument(std::string name) {};
     Model& m_model;
-    View* m_view;
 };
 
-void Controller::execute() {
+void Controller::test1() 
+{
     m_model.newDocument("Doc-1");
-    m_model.newRectangle("Прямоугольник-1", 12, 13);
-    m_model.newCircle("Окружность-1", 33);
+    m_model.newRectangle(12, 13);
+    m_model.newCircle(33);
+    m_model.newCircle(124);
+    m_model.print();
+}
 
-    if (m_view != nullptr)
-        m_view->view(m_model);
-    // m_model.print();
+void Controller::command(std::string line) 
+{
+    if (line == "new document") {
+        std::cout << "Введите имя документа: ";
+        std::string ss_name;
+        if (std::getline(std::cin, ss_name) && !ss_name.empty()) {
+            m_model.newDocument(ss_name);
+        }
+    }
+    else if (line == "open document") {
+        std::cout << "Введите имя документа: ";
+        std::string ss_name;
+        if (std::getline(std::cin, ss_name) && !ss_name.empty()) {
+
+        }
+    }
+    else if (line == "save document") {
+        // m_model.
+    }
+    else if (line == "new rectangle") {
+        
+    }
+    else if (line == "new circle") {
+        std::cout << "Введите радиус окружности: ";
+        int r   = 0;
+        std::string ss_radius;
+        if (std::getline(std::cin, ss_radius) && !ss_radius.empty()) {
+            std::istringstream oss(ss_radius);
+            oss >> r;
+            m_model.newCircle(r);
+        }
+    }
+    else if (line == "delete object") {
+        std::cout << "Введите идентификатор объекта: ";
+        int id = 0;
+        std::string ss_id;
+        if (std::getline(std::cin, ss_id) && !ss_id.empty()) {
+            std::istringstream oss(ss_id);
+            oss >> id;
+            m_model.deleteObject(id);
+        }
+    }
+    else {
+        std::cout << "Нет такой команды\n";
+    }
+}
+
+//----------------------------------------------------------------
+
+class View {
+public:
+    View(Controller& controller) : m_controller(controller) {};
+    void view(const Model& model) {
+        model.print();
+    };
+    void execute();
+private:
+    Controller& m_controller;
+};
+
+void View::execute() {
+    std::string line;
+    while (std::getline(std::cin, line) && !line.empty()) {
+        m_controller.command(line);
+    }
 }
 
 //----------------------------------------------------------------
@@ -135,17 +202,14 @@ int main()
 {
     Model model;
     Controller controller(model);
-    View viewer;
-    // controller.setView(&viewer);
+    View viewer(controller);
+    controller.test1();
 
+    // auto update = [](){ std::cout << "f_update\n"; };
 
-    controller.execute();
+    viewer.execute();
 
-    // Rectangle rect("rect1", 12, 10);
-    // std::cout << rect << std::endl;
-
-    // Circle circle("asdald", 33);
-    // std::cout << circle << std::endl;
+    viewer.view(controller.getModel());
 
     return 0;
 }
